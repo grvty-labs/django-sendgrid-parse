c2bf28024226a74681bc2824ba4416c7ae5b81bf
letops <daniel.ortizp91@gmail.com>
2016-07-28 14:36:17 -0500

    Fixes

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ import os
 from setuptools import setup, find_packages
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+with open('README.md') as f:
+    readme = f.read()
 
 setup(
     name='django-sendgrid-parse',
-    # packages=['django_sendgrid_parse'],
-    packages=find_packages(),
-    data_files=['README.md'],
-    version='0.1.7',
+    version='0.1.8',
     description='Django app to receive and save incoming email\
 notification events from sendgrid to our database',
-    # long_description=readme,
-    long_description=read("README.md"),
+    long_description=readme,
+    # long_description=read("README.md"),
     author='GRVTYlabs',
     author_email='daniel.ortiz@grvtylabs.com',
     url='https://github.com/letops/django-sendgrid-parse',
+    packages=find_packages(),
+    # packages=['django_sendgrid_parse'],
+    license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
