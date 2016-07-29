28d21371580165bd75f3ec35242859d5317ba1ad
letops <daniel.ortizp91@gmail.com>
2016-07-29 15:05:32 -0500

    Added FROM in model. Improved views and forms.

--- django_sendgrid_parse/forms.py
+++ django_sendgrid_parse/forms.py
@@ -4,8 +4,24 @@ from .models import Email
 
 
 class EmailForm(forms.ModelForm):
-    attachments = forms.IntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super(EmailForm, self).__init__(*args, **kwargs)
+        self.fields['to'] = forms.CharField()
+        self.fields['from'] = forms.CharField()
+        self.fields['attachments'] = forms.IntegerField()
+        self.fields['to_mailbox'].required = False
+        self.fields['from_mailbox'].required = False
+
+    def clean(self, *args, **kwargs):
+        super(EmailForm, self).clean()
+        self.cleaned_data['to_mailbox'] = self.cleaned_data.get('to')
+        self.cleaned_data['from_mailbox'] = self.cleaned_data.get('from')
 
     class Meta:
         model = Email
         exclude = ('attachments', )
+        widget = {
+            'to_mailbox': forms.HiddenInput(),
+            'from_mailbox': forms.HiddenInput(),
+        }
